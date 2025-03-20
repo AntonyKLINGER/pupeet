cda8b5490d59f2e357e6eaa9342912759d2452c9
AntonyKLINGER <klingerantony@gmail.com>
2025-03-20 09:21:25 +0100

    maj args

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,7 @@ import chromium from 'chrome-aws-lambda';
 
 export const config = {
   api: {
-    bodyParser: false, // Désactiver l'analyse automatique de Next.js pour les requêtes POST
+    bodyParser: false,
   }
 };
 
@@ -20,7 +20,6 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Méthode non autorisée' });
   }
 
-  // On récupère le body brut de la requête et on le parse en JSON
   const rawBody = await buffer(req);
   const { htmlContent } = JSON.parse(rawBody.toString());
 
@@ -29,32 +28,35 @@ export default async function handler(req, res) {
   }
 
   try {
-    // Lancement de Puppeteer
     const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath || '/usr/bin/google-chrome-stable',
+      args: [
+        ...chromium.args,
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--single-process',
+        '--disable-gpu'
+      ],
+      executablePath: await chromium.executablePath,
       headless: true,
     });
 
     const page = await browser.newPage();
 
-    // Définir les dimensions de la page
     await page.setViewport({ width: 800, height: 600 });
 
-    // Charger le contenu HTML
     await page.setContent(htmlContent, { waitUntil: 'load' });
 
-    // Prendre une capture d'écran
     const imageBuffer = await page.screenshot({ type: 'png' });
 
     await browser.close();
 
-    // Retourner l'image
     res.setHeader('Content-Type', 'image/png');
     res.status(200).send(imageBuffer);
 
   } catch (error) {
     console.error('Erreur lors de la génération de l\'image :', error.message);
-    res.status(500).json({ message: 'Erreur lors de la génération de l\'image', error: error.message });
+
+    return res.status(500).json({ message: 'Erreur lors de la génération de l\'image', error: error.message });
   }
 }
